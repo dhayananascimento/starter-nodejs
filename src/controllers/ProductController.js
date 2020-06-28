16f410ff7cfd863be5df40dbcecd7a40e454c68e
Dhayana <contatodhayana@gmail.com>
2020-06-28 00:07:23 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,18 +4,26 @@ const Product = mongoose.model('Product')
 module.exports = {
     async index(req, res) {
         const products = await Product.find()
-
         return res.json(products)
-    }
+    },
+
+    async show(req, res) {
+        const product = await Product.findById(req.params.id)
+        return res.json(product)
+    },
+
+    async store(req, res) {
+        const product = await Product.create(req.body)
+        return res.json(product)
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        return res.json(product)
+    },
+
+    async destroy(req, res) {
+        await  Product.findByIdAndRemove(req.params.id)
+        return res.send()
+    },
 }
-
-
-
-// (req, res) => {
-// 	// Product.create({
-// 	// 	title: 'React Native', 
-// 	// 	description: 'Build native apps with React',
-// 	// 	url: 'http://github.com/facebook/react-native'
-// 	// })
-// 	return res.send("Hello World!")
-// }
